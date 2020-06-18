b0f4bf4443d286c142363a513f1ff61545e40e14
ruchitha123120 <67085226+ruchitha123120@users.noreply.github.com>
2020-06-18 20:55:39 +0530

    Add files via upload

---
+++ Assignment1_simple interest.py
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "simple interest is 360.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "P = 150\n",
+    "R = 12\n",
+    "T = 20\n",
+    "SI = (P * R * T) / 100\n",
+    "print(\"simple interest is\", SI)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
