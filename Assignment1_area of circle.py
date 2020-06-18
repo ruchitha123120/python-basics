2a828287f5ed10a6d68026821fe99aed9c8d2e76
ruchitha123120 <67085226+ruchitha123120@users.noreply.github.com>
2020-06-18 20:56:32 +0530

    Add files via upload

---
+++ Assignment1_area of circle.py
@@ -0,0 +1,57 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      " Please Enter the radius of a circle: 5\n",
+      " Area Of a Circle = 78.50\n",
+      " Circumference Of a Circle = 31.40\n"
+     ]
+    }
+   ],
+   "source": [
+    "PI = 3.14\n",
+    "radius = float(input(' Please Enter the radius of a circle: '))\n",
+    "area = PI * radius * radius\n",
+    "circumference = 2 * PI * radius\n",
+    "\n",
+    "print(\" Area Of a Circle = %.2f\" %area)\n",
+    "print(\" Circumference Of a Circle = %.2f\" %circumference)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
