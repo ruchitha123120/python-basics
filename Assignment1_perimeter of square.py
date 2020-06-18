0b8dfabd31db4e164754d1f138120c7bf4ce7e28
ruchitha123120 <67085226+ruchitha123120@users.noreply.github.com>
2020-06-18 20:58:33 +0530

    Add files via upload

---
+++ Assignment1_perimeter of square.py
@@ -0,0 +1,59 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Enter 'x' for exit.\n",
+      "Enter side length of square: 8\n",
+      "\n",
+      "Perimeter of Square: 32\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Enter 'x' for exit.\");\n",
+    "side = input(\"Enter side length of square: \");\n",
+    "if side == 'x':\n",
+    "    exit();\n",
+    "else:\n",
+    "    slength = int(side);\n",
+    "    perimeter = 4*slength;\n",
+    "    print(\"\\nPerimeter of Square:\", perimeter);"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
