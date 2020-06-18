b1444bac0e08997eae9ce6db9e240d4e4804f3b3
ruchitha123120 <67085226+ruchitha123120@users.noreply.github.com>
2020-06-18 20:57:04 +0530

    Add files via upload

---
+++ Assignment1_area of traingle.py
@@ -0,0 +1,54 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "The area of the triangle is 4.68\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = 2\n",
+    "b = 5\n",
+    "c = 6\n",
+    "s = (a + b + c) / 2\n",
+    "area = (s*(s-a)*(s-b)*(s-c)) ** 0.5\n",
+    "print('The area of the triangle is %0.2f' %area)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
