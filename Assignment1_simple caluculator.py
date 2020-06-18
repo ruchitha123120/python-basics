ef8df69fa4b90d2602d9d4f83c5bac7e0e6001da
ruchitha123120 <67085226+ruchitha123120@users.noreply.github.com>
2020-06-18 20:55:02 +0530

    Add files via upload

---
+++ Assignment1_simple caluculator.py
@@ -0,0 +1,72 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Enter First number: 12\n",
+      "Enter Second number 5\n",
+      "Sum of  12 and 5 is : 17\n",
+      "Difference of  12 and 5 is : 7\n",
+      "Product of 12 and 5 is : 60\n",
+      "Division of  12 and 5 is : 2.4\n",
+      "Floor Division of  12 and 5 is : 2\n",
+      "Exponent of  12 and 5 is : 248832\n",
+      "Modulus of  12 and 5 is : 2\n"
+     ]
+    }
+   ],
+   "source": [
+    "num1 = int(input('Enter First number: '))\n",
+    "num2 = int(input('Enter Second number '))\n",
+    "add = num1 + num2\n",
+    "sub = num1 - num2\n",
+    "mul = num1 * num2\n",
+    "div = num1 / num2\n",
+    "floor_div = num1 // num2\n",
+    "expo = num1 ** num2\n",
+    "modulus = num1 % num2\n",
+    "print('Sum of ',num1 ,'and' ,num2 ,'is :',add)\n",
+    "print('Difference of ',num1 ,'and' ,num2 ,'is :',sub)\n",
+    "print('Product of' ,num1 ,'and' ,num2 ,'is :',mul)\n",
+    "print('Division of ',num1 ,'and' ,num2 ,'is :',div)\n",
+    "print('Floor Division of ',num1 ,'and' ,num2 ,'is :',floor_div)\n",
+    "print('Exponent of ',num1 ,'and' ,num2 ,'is :',expo)\n",
+    "print('Modulus of ',num1 ,'and' ,num2 ,'is :',modulus)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
