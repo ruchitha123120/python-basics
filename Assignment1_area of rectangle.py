e79f5e917088ebe0f3b832c7c9f508e385859573
ruchitha123120 <67085226+ruchitha123120@users.noreply.github.com>
2020-06-18 20:58:08 +0530

    Add files via upload

---
+++ Assignment1_area of rectangle.py
@@ -0,0 +1,57 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      " Enter the Length of a Triangle: 5\n",
+      " Enter the Width of a Triangle: 7\n",
+      "The Area of a Rectangle using 5.0 and 7.0  =  35.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "length = float(input(' Enter the Length of a Triangle: '))\n",
+    "width = float(input(' Enter the Width of a Triangle: '))\n",
+    "\n",
+    "# calculate the area\n",
+    "area = length * width\n",
+    "\n",
+    "print(\"The Area of a Rectangle using\", length, \"and\", width, \" = \", area)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
