41c9acb1aa47de65ab3ec6cd53fb544c1d52216f
ruchitha123120 <67085226+ruchitha123120@users.noreply.github.com>
2020-06-18 20:57:43 +0530

    Add files via upload

---
+++ Assignment1_celsius to fahrenheit.py
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "90.0 degree Celsius is equal to 194.0 degree Fahrenheit\n"
+     ]
+    }
+   ],
+   "source": [
+    "celsius = 90\n",
+    "fahrenheit = (celsius * 1.8) + 32\n",
+    "print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))\n",
+    "\n",
+    "celsius = (fahrenheit - 32) / 1.8\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
